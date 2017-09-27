97514bfc0f978c2ad0295ae6d8d1613ea19741a8
mengshangqi <mengshangqis@gmail.com>
2017-09-27 21:33:53 +0800

    init Mison

---
+++ src/Bit.h
@@ -0,0 +1,47 @@
+#ifndef MISON_SRC_BIT_H
+#define MISON_SRC_BIT_H
+
+#include <string>
+#include <vector>
+#include <memory>
+#include "type.h"
+#include <string>
+
+namespace meng {
+using std::string;
+using std::shared_ptr;
+using std::move;
+using std::vector;
+class BitMap {
+public:
+    string to_string();
+    BitMap(vector<i64> &&bits);
+    size_t length() {
+        return bits.size();
+    }
+    void push(i64 sta) {
+        this->bits.push_back(sta);
+    }
+    const i64& operator[](size_t i) {
+        return bits[i];
+    }
+private:
+    vector<i64> bits;
+};
+
+inline i64 R(i64 x) {
+    return x & (x - 1);
+}
+
+inline i64 E(i64 x) {
+    return x & (-x);
+}
+
+inline i64 S(i64 x) {
+    return x ^ (x - 1);
+}
+
+BitMap Building_Structural_Character_Bitmaps(const string &str, u8 ch);
+}
+
+#endif
---
+++ src/type.h
@@ -0,0 +1,14 @@
+#ifndef MISON_SRC_TYPE_H_
+#define MISON_SRC_TYPE_H_
+
+using u8 = unsigned char;
+using u16 = unsigned short;
+using u32 = unsigned int;
+using u64 = unsigned long long;
+
+using i8 = char;
+using i16 = short;
+using i32 = int;
+using i64 = long long;
+
+#endif
