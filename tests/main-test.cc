d73bc3d9ff0fc0a43c1a01d7df6f76fe385cb0ee
mengshangqi <mengshangqis@gmail.com>
2017-09-29 19:22:03 +0800

    checkout dev

--- tests/main-test.cc
+++ tests/main-test.cc
@@ -1,9 +1,11 @@
 #include <iostream>
 #include "gtest/gtest.h"
 #include "Bit.h"
+#include <cassert>
 using namespace std;
 int main(int argc, char** argv) {
   testing::InitGoogleTest(&argc, argv);
-  RUN_ALL_TESTS();
+  int ret = RUN_ALL_TESTS();
+  assert(ret==0);
   return 0;
 }
